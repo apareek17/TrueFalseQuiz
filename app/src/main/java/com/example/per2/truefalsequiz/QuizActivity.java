da76f4bef626bc9d3d2f932f7d40879c5c283fbd
Ayush Pareek <ayush7702@gmail.com>
2018-10-08 09:35:42 -0700

    Initial commit

---
+++ app/src/main/java/com/example/per2/truefalsequiz/QuizActivity.java
@@ -0,0 +1,54 @@
+package com.example.per2.truefalsequiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.gson.Gson;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+
+public class QuizActivity extends AppCompatActivity {
+    public static final String TAG = "QuizActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+
+        InputStream stream = getResources().openRawResource(R.raw.questions); // getting XML
+
+        String jsonstring = readTextFile(stream);
+        Log.d(TAG, "onCreate ", + jsonString);
+
+        // create a gson object
+        Gson gson = new Gson();
+        // read your json file into an array of questions
+        Question[] questions =  gson.fromJson(jsonString, Question[].class);
+        // convert your array to a list using the Arrays utility class
+        List<Question> questionList = Arrays.asList(questions);
+// verify that it read everything properly
+        Log.d(TAG, "onCreate: " + questionList.toString());
+    }
+
+    public String readTextFile(InputStream inputStream) {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+        byte buf[] = new byte[1024];
+        int len;
+        try {
+            while ((len = inputStream.read(buf)) != -1) {
+                outputStream.write(buf, 0, len);
+            }
+            outputStream.close();
+            inputStream.close();
+        } catch (IOException e) {
+
+        }
+        return outputStream.toString();
+
+    }
+}
