b76593bc0f5d43b401f64d39f6b6851b6ca3f4cd
Ayush Pareek <ayush7702@gmail.com>
2018-10-26 09:34:02 -0700

    yeet

---
+++ app/src/main/java/com/example/per2/truefalsequiz/Quiz.java
@@ -0,0 +1,71 @@
+package com.example.per2.truefalsequiz;
+
+import java.util.List;
+
+public class Quiz {
+    private List<Question> questionList;
+    private int score = 0;
+    private int questionNumber;
+    private int AllQuestions = 3;
+
+    public Quiz() {
+    }
+
+    public Quiz(List<Question> questionList) {
+        this.questionList = questionList;
+    }
+
+    public int getQuestionNumber(){
+        return questionNumber;}
+
+    public void setQuestion(List<Question> question) {
+        this.questionList = question;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+
+    public boolean isTrue(){
+        boolean correct = true;
+        boolean answer = getQuestion().getAnswer();
+        return correct==answer;
+
+    }
+
+    public Question getQuestion(){
+        Question s;
+
+        s = questionList.get(questionNumber);
+        return s;
+    }
+
+
+    public String getNextQuestionText(){
+        return getNextQuestion().getQuestionText();
+    }
+
+    public Question getNextQuestion() {
+        Question s = null;
+        if (hasMoreQuestions()) {
+            s = questionList.get(questionNumber + 1);
+            questionNumber++;
+
+        }
+        return s;
+    }
+
+
+    public Boolean hasMoreQuestions(){
+            boolean x = true;
+            if (questionNumber + 1 > AllQuestions) {
+                x = false;
+            }
+            return x;
+        }
+}
